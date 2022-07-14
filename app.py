408d0ebad816e76dc37d237604dfb4888590d0be
Yurley1998 <81891273+Yurley1998@users.noreply.github.com>
2022-07-14 11:03:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -109,5 +109,5 @@ st.write("## Estratos")
 opc = st.selectbox(label="", 
                    options =[1, 2, 3, 4, 5, 6])
 dfe= datos[(datos['estrato']==opc)]
-fig4= px.histogram(dfe, x="servicio_internet", color_discrete_sequence=["#86C7BE"], rwidth=0.70)
+fig4= px.histogram(dfe, x="servicio_internet", color_discrete_sequence=["#86C7BE"], width=0.70)
 st.plotly_chart(fig4)
