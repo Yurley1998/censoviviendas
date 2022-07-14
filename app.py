10b4c887b07f929ff1f16b9d78248d87b43d1613
Yurley1998 <81891273+Yurley1998@users.noreply.github.com>
2022-07-13 21:19:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -52,10 +52,10 @@ st.plotly_chart(
 def graficobarras1(datos):
     
     fig = px.bar(
-        datos.groupby(["estrato"])        
-        color_discrete_sequence=["#86C7BE","white"],
+        datos,  
         x ="estrato",
-        y ="servicio_internet"
+        y ="servicio_internet",
+        color= 'medal'
     )
     return fig
 varfig = graficobarras1(datos)
